0005bd923254f43cd74f73f8378d651c2aa4deb5
kikeval <valero81_2@hotmail.com>
2017-03-24 23:01:56 +0000

    arreglo de pdf exporter 3

--- model/decrimdata.js
+++ model/decrimdata.js
@@ -314,7 +314,7 @@ var DecrimData = function () {
     
     function base64_encode(file) {
         // read binary data
-        var bitmap = fs.readFileSync(file);
+        var bitmap = instance.fs.readFileSync(file);
         // convert binary data to base64 encoded string
         return new Buffer(bitmap).toString('base64');
     }
