becd4bbb4d830947ba7d9c7fa546aff301091aaf
kikeval <valero81_2@hotmail.com>
2017-07-03 03:43:27 +0000

    Solving the email sending

--- model/gmailmanager.js
+++ model/gmailmanager.js
@@ -1,6 +1,5 @@
 var ResponseManager = require("../model/responsemanager.js")
 var Connection = require("../model/connection.js")
-var Mailjet = require('node-mailjet').connect('01c42d863084eebd93ebecd5491759f9', 'ef6d6e9e7c0414b236910b08d753ba83');
 //Clase GMailManager
 var GMailManager = function () {
     var user = "developer.aminds@gmail.com";
@@ -20,14 +19,23 @@ var GMailManager = function () {
     
     this.sendEmail = function(data, responseCallback) {
         console.log(data);
+        var Mailjet = require('node-mailjet').connect('01c42d863084eebd93ebecd5491759f9', 'ef6d6e9e7c0414b236910b08d753ba83');
         
-        var sendEmail = Mailjet.post('send');
-        sendEmail.request(data).then(function () {
+        var sendEmail = Mailjet.post('send').request(data);
+        sendEmail.on('success', function (response, body) {
+            console.log("Response:");
+            console.log(response);
+            console.log("Body:");
+            console.log(body);
             var responseManager = new ResponseManager();
             responseManager.object = "Un correo con la información requerida ha sido enviada.";
             responseManager.error = "NO_ERROR";            
             responseCallback(responseManager);
-        }).catch(function (err) {
+        }).on('error', function (err, response) {
+            console.log("Response:");
+            console.log(response);
+            console.log("Error:");
+            console.log(err);
            var responseManager = new ResponseManager();
            responseManager.object = "Hay un error con el servidor de correos, intente más tarde";
             responseManager.error = err;
